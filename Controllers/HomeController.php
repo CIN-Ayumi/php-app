e930edd58a6f043c7700cc6ee581c55ddbd9a6d5
CIN-Ayumi <155986196+CIN-Ayumi@users.noreply.github.com>
2024-02-15 15:16:54 +0900

    Add files via upload

---
+++ Controllers/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+require_once ROOT_PATH.'Controllers/Controller.php';
+
+class HomeController extends Controller
+{
+    public function index(){
+        $this->view('home/index');
+    }
+}
+?>
